4e94cff184dfbb6240b7c512a6bf44bc7ab3d4cd
Yarilo <contact@yarilovillanueva.com>
2022-04-22 18:00:09 +0200

    Client scaffolding
    Moving server code into a separate folder and using the quickstart template of svelte (https://github.com/sveltejs/template) to bootstrap the client

---
+++ client/src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let name = 'world';
+</script>
+
+<div class='title'>
+	<h1>Azar</h1>
+</div>
+
+<style>
+	 .title {
+		 display:flex;
+		 justify-content: center;
+		 align-items: center;
+		 height: 100%;
+		 width: 100%;
+	 }
+</style>
