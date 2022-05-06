0fce4d8c782e1a705329ea59e84843dbec327740
Yarilo <contact@yarilovillanueva.com>
2022-05-06 18:24:46 +0200

    Fetch the events from server and display them in the page
    As first approach to communicate between client & server

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,40 @@
 <script lang="ts">
-	let name = 'world';
+    import * as axios from 'axios';
+	import {onMount} from 'svelte';
+
+	let request;
+	let currentEvents = [];
+
+	let BASE_SERVER_URL = 'http://localhost:4242'
+	onMount(async () => {
+        request = axios.create({
+			baseURL: BASE_SERVER_URL,
+		});
+		 const response = await request.get(`/events`);
+		 currentEvents = response.data;
+		 console.log('CURRENT EVENTS', currentEvents)
+       })
+
 </script>
 
-<div class='title'>
-	<h1>Azar</h1>
+<div class='layout'>
+	<div class='title'>
+		<h1>Azar</h1>
+	</div>
+	<ul class='event-list'>
+		{#each currentEvents as event}
+		<li>
+			<h2>{event.title}</h2>
+			<p>{event.description}</p>
+		</li>
+		{/each}
+	</ul>
 </div>
 
 <style>
-	 .title {
+	 .layout {
 		 display:flex;
+		 flex-direction: column;
 		 justify-content: center;
 		 align-items: center;
 		 height: 100%;
