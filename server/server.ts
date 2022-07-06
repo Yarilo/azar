8e2e24e2a27fb3c4f0d99f18901957027b0344b1
Yarilo Villanueva <contact@yarilovillanueva.com>
2022-07-06 17:43:35 +0200

    Add a more complete logger

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import logger from "https://deno.land/x/oak_logger/mod.ts";
 import db from "./db.ts";
 import { auth, events, places } from "./routes/index.ts";
 
@@ -25,19 +26,9 @@ app.use(
   }),
 );
 
-// Logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
-// Timing
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-});
+
+app.use(logger.logger)
+app.use(logger.responseTime)
 
 app.use(router.routes());
 app.use(router.allowedMethods());
