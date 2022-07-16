111836fa77b4d94127051a045ae530e2319558d3
Yarilo Villanueva <contact@yarilovillanueva.com>
2022-07-16 18:11:27 +0200

    URL in scraper to env

--- scraper/src/constants.ts
+++ scraper/src/constants.ts
@@ -1,6 +1,7 @@
 
+const { AZAR_SERVER_URL = '' } = process.env;
 
-export const BASE_URL = 'https://azar.deno.dev'; // @TODO: To env
+export const BASE_URL = AZAR_SERVER_URL;
 export const enum URLS {
     PLACES = '/places',
     EVENTS = '/events',
